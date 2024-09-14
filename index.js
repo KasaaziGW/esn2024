94dbeb7e8cb70926b245e5700513484e8dd43bd1
KasaaziGW <kasaazi.gw@gmail.com>
2024-09-14 16:13:02 +0300

    Made changes to the frontend

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const PORT = 3300; // the port where our server will be running
 
 // instantiating express
 const app = express();
+app.use(express.static("public")); // points to where the static files are.
 // set up the body-parse utility
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // setting the templating engine
 app.set("view engine", "ejs");
 
+// connecting to the database
+const dbUrl =
+  "mongodb+srv://gkasaazi:6nhq1kidDxCiynU1@cluster0.9px3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// mongoose.connect(dbUrl, (err) => {
+//   if (err) console.log(`Couldn't connect to MongoDB\n${err}`);
+//   else console.log("Successfully connected to MongoDB");
+// });
+// mongoose.connect()
+try {
+  mongoose.connect(dbUrl);
+  console.log("Successfully connected to MongoDB");
+} catch (error) {
+  // handleError(error);
+  console.log(`Couldn't connect to MongoDB\n${error}`);
+}
 // loading the login page
 app.get("/", (request, response) => {
   response.render("login");
 });
 
+// loading the register page
+app.get("/register", (request, response) => {
+  response.render("register");
+});
+
+// registering a user
+app.post("/userRegister", (request, response) => {
+  // getting the data from the user
+  let email = request.body.email;
+  let fullname = request.body.fullname;
+  let pswd = request.body.password;
+  let cpswd = request.body.confirmpassword;
+  response.send(
+    `Email: ${email}\nFullname: ${fullname}\nPassword: ${pswd}\nPassword 2: ${cpswd}`
+  );
+  // response.send("Email " + email + " ");
+});
 // listen for incoming connections
 app.listen(PORT, () => {
   console.log(`The server is up and running on port ${PORT}`);
