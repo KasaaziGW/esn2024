9b52509f79767737313cf31f84ec4201f3b1e39f
esd-coder <mseddie5@gmail.com>
2024-10-28 15:57:07 +0300

    Updated index.js

--- models/Citizen.js
+++ models/Citizen.js
@@ -4,6 +4,16 @@ const citizenSchema = new Schema({
   username: { type: String },
   fullname: { type: String },
   password: { type: String },
+  privilege: {
+    type: String,
+    enum: ['Administrator', 'Coordinator', 'Citizen'],
+    default: 'Citizen'
+  },
+  status: {
+    type: String,
+    enum: ['OK', 'Active', 'Inactive'],
+    default: 'Active'
+  }
 });
 const Citizen = mongoose.model("Citizen", citizenSchema);
 module.exports = { Citizen };
